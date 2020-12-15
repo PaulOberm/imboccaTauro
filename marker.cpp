c1cab25c164db389c08f60cac899512ec4ca6d44
MXN <paul.obermayer92@gmail.com>
2020-12-15 21:49:34 +0000

    Add numerical visualization of marker number on machine front-end

--- marker.cpp
+++ marker.cpp
@@ -2,24 +2,53 @@
 #include <QThread>
 #include <QDebug>
 
-Marker::Marker()
+/**
+ * @brief Marker::Marker
+ * A marker class to determine the Feeder's position
+ *
+ */
+Marker::Marker(QLCDNumber* lcd)
 {
     // Set default GPIO pin
     pin_number = 27;
+    ui_lcd = lcd;
 }
 
-Marker::Marker(int gpio_pin){
+Marker::Marker(int gpio_pin, QLCDNumber* lcd){
     pin_number = gpio_pin;
+    ui_lcd = lcd;
 }
 
+bool Marker::istoggle(int prev, int actual){
+    if(prev==actual){
+        return false;
+    }else {
+        return true;
+    }
+}
+
+// Replace with interrupt
+/**
+ * @brief Marker::infiniteRead
+ * The marker sensors are polled continuously.
+ * In combination with the motor direction
+ * the counting of the position is done.
+ */
 void Marker::infiniteRead(){
     int increment = 0;
-    int previous_increment = 1;
+    int previous_increment = 0;
     for (;;) {
              increment = poll();
-             // if istoggle(increment, previous_increment)
-             // MARKER_COUNT += ...
+             if(istoggle(previous_increment, increment)){
+                 // Update position when toggled
+                 // either by 1: forward, 0: marker passed,
+                 // -1: backward
+                 MARKER_COUNT += increment;
+                 previous_increment = increment;
+             }
              // Write new marker count value to LCD in front-end
+             // qDebug() << MARKER_COUNT;
+             ui_lcd->display(MARKER_COUNT);
       }
 }
 
@@ -40,6 +69,8 @@ int Marker::poll() {
     bool pin_pressed = false;
     char pin_count[2] = "0";
     const char pressed_string[2] = "0";
+
+    // HARDCODED pin 27
     if((sysfs_handle = fopen("/sys/class/gpio/gpio27/value", "r")) != NULL)
     {
             fread(pin_count, sizeof(char), 1, sysfs_handle);
@@ -55,10 +86,9 @@ int Marker::poll() {
     // Return new count update based on direction of motor
 
     if(pin_pressed){
-        qDebug()  << "Console Mode. Marker detected!\n";
-        qDebug() << pin_count;
+        // qDebug()  << "Console Mode. Marker detected!\n";
         // Compare with driving direction to increase or decrease counter
-        return int(pin_count);
+        return 1;
     }
-    return int(pin_count);
+    return 0;
 }
